7a42e3c792bfa101e223c4c5c32007240237324e
cabamaximiliano <cabamaximiliano@gmail.com>
2018-12-30 13:22:57 -0300

    agregado de enunciado de segundo parcial

---
+++ programacion1/Simulacro3.py
@@ -0,0 +1,12 @@
+"""
+SIMULACRO DE PARCIAL
+
+Desarrolle un programa que solicite al usuario el ingreso de una
+palabra y la busque en un archivo de texto cualquiera imprimiendo
+todas las oraciones donde se encuentre.
+Tener encuenta que:
+1- el archivo de texto puede ser cualquiera
+2- se considera oracion a una serie de una o mas palabras termiandas con un punto
+3- no se permite cargar el archivo completo en memoria
+"""
+
